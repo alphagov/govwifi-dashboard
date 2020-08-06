276ec8ca3c49ce648627825349b640460ec823a5
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-08-06 15:23:50 +0100

    fix: make the logging api controller fetch the latest file available
    The metrics generators don't override the same file in S3, they add
    the date in the name and S3 adds a timestamp anyway.
    
    Use that timestamp to fetch the latest file available for the given period.

--- app/controllers/watchdog/logging_api_controller.rb
+++ app/controllers/watchdog/logging_api_controller.rb
@@ -18,11 +18,27 @@ module Watchdog
 
     private
 
-    def extract_data_for(period)
-      raw = S3_BUCKET.get_object(
-        bucket: 'govwifi-mock-metrics',
-        key: 'active_users.json'
+    def get_latest_file_for(period)
+      files = S3_BUCKET.list_objects_v2(
+        bucket: 'govwifi-staging-metrics-bucket',
+        prefix: "active-users-#{period}"
       )
+
+      latest = files
+               .contents
+               .sort_by(&:last_modified)
+               .reverse
+               .first
+               .key
+
+      S3_BUCKET.get_object(
+        bucket: 'govwifi-staging-metrics-bucket',
+        key: latest
+      )
+    end
+
+    def extract_data_for(period)
+      raw = get_latest_file_for(period)
 
       data = JSON.parse(raw.body.read)
 
